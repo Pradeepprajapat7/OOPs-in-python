dd6cf67bcce06d8bc4f456069996cd18fc2e3e8b
Pradeep <mr.pradeepkumhar6264@gmail.com>
2026-05-22 14:32:59 +0530

    create a login and signup

--- OOPs-in-python/oops_proj.py
+++ OOPs-in-python/oops_proj.py
@@ -13,14 +13,38 @@ class chatbook:
                            4. press 4 send a message
                            5. press 5 for exit  """)
         if user_input == "1":
-            pass
+            self.signup()
         elif user_input == "2":
-            pass    
+            self.signin()   
         elif user_input == "3":
             pass
         elif user_input == "4":
             pass
-        elif user_input == "5":
-            print("thank you for using chatbook")
+        else:
+            exit()
+    
+    def signup(self):
+        email = input("enter your email-->>")
+        password = input("enter your password-->>")
+        self.username = email
+        self.password = password
+        print("you are singup successfully! ")
+        print("\n")
+        self.menu()
+    
+    def signin(self):
+        if self.username == "" and  self.password == "":
+            print("you are not signup yet! ")
+        else:
+            uname = input("enter your email-->>")
+            password = input("enter your password-->>") 
+            if self.username == uname and self.password == password:
+                print("you are login successfully! ")
+                self.logging = True
+
+            else:
+                print("invalid username or password! ")
+        print("\n")
+        self.menu()
 
 obj = chatbook()
