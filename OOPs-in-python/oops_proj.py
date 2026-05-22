971a2c573f28cd40d422d322df25691ae55dd079
Pradeep <mr.pradeepkumhar6264@gmail.com>
2026-05-22 15:19:31 +0530

    complete the project

--- OOPs-in-python/oops_proj.py
+++ OOPs-in-python/oops_proj.py
@@ -10,16 +10,16 @@ class chatbook:
                            1. press 1 for signup
                            2. press 2 for login
                            3. press 3 for post
-                           4. press 4 send a message
+                           4. press 4 send a message to your followers
                            5. press 5 for exit  """)
         if user_input == "1":
             self.signup()
         elif user_input == "2":
             self.signin()   
         elif user_input == "3":
-            pass
+            self.my_post()
         elif user_input == "4":
-            pass
+            self.send_message()
         else:
             exit()
     
@@ -47,4 +47,24 @@ class chatbook:
         print("\n")
         self.menu()
 
+
+    def my_post(self):
+        if self.logging == True:
+            txt = input("enter your message here -->>")
+            print(f"your message is posted your followers can see it!-->>{txt}")
+
+        else:
+            print("you need to login first! ")
+            print("\n")
+            self.menu()
+    
+    def send_message(self):
+        if self.logging == True:
+            txt = input("enter your message here -->>")
+            followers = input("enter your followers name here -->>")
+            print(f"your message is sent to {followers}!")
+        else:
+            print("you need to login first! ")
+            print("\n")
+            self.menu()
 obj = chatbook()
